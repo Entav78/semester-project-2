5771edec18861de7a4b119cf1229b57872103955
Entav78 <152163293+Entav78@users.noreply.github.com>
2025-01-29 21:03:13 +0100

    Saved all changes before moving project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './pages/**/*.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
